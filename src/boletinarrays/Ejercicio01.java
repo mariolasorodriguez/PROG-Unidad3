730cf8907693653e5f62a28f7e0f989c4bd972c1
Mario <mario.laso@iesnervion.es>
2025-12-04 14:18:33 +0100

    04/12

---
+++ src/boletinarrays/Ejercicio01.java
@@ -0,0 +1,20 @@
+package boletinarrays;
+
+public class Ejercicio01 {
+	public static void main(String[] args) {
+
+		// Crea una tabla de enteros de longitud 10 que se inicializará con números
+		// aleatorios comprendidos entre 1 y 100.
+		int[] tabla = new int[10];
+		
+		for (int i = 0; i < tabla.length; i++) {
+			tabla[i] = (int) (Math.random() * 100) + 1;
+		}
+		
+		// Muestra el contenido de la tabla en pantalla.
+		for (int i = 0; i < tabla.length; i++) {
+			System.out.println("Elemento en la posición " + i + ": " + tabla[i]);
+		}
+		
+	}
+}
